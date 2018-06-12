09980100d5e85f613de8c9cc2422a255c939a0fd
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-06-11 23:34:04 -0500

    Problem 4 Complete

--- app.js
+++ app.js
@@ -82,12 +82,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumArray(sumArr) {
-
+  var sum01 = sum(sumArr[0], sumArr[1])[0];
+  var sum012 = sum(sum01, sumArr[2])[0];
+  var theString = sumArr + ' was passed in as an array of numbers, and ' + sum012 + ' is their sum.';
+  return [sum012, theString];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray([2, 3, 4]);
+testSumArray([2, 3, 4]);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
